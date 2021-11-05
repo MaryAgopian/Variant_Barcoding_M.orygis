0f295920d29ad1b49dfb02f250bba48eac067106
MaryAgopian <84090265+MaryAgopian@users.noreply.github.com>
2021-11-05 01:17:13 -0400

    Update and rename samtools.pl to 3) samtools.pl

--- 3) samtools.pl
+++ 3) samtools.pl
@@ -1,7 +1,8 @@
 # Using samtools
 
 # samtools is a software used to manipulate SAM files. 
-# In this case, it was used to transform our previously generated SAM files into BAM files. BAM is a binary version of the text SAM file. We convert SAM to BAM since the binary format is easier for the programs to process and work with.
+# In this case, it was used to transform our previously generated SAM files into BAM files. BAM is a binary version of the text SAM file. 
+# We convert SAM to BAM since the binary format is easier for the programs to process and work with.
 # We next converted out BAM files into their sorted version, known as sorted.BAM. Sorting allows us to sort the data according to the scaffolding seen in our reference genome. 
 # This allows for significant SNPs to be identified instead of random differences that are detected based on the non-sorted alignment.
 
@@ -16,7 +17,7 @@ samtools view -bT Mycobacterium_tuberculosis_h37rv.fna aln4-pe.sam > aln4.bam
 samtools view -bT Mycobacterium_tuberculosis_h37rv.fna aln5-pe.sam > aln5.bam
 samtools view -bT Mycobacterium_tuberculosis_h37rv.fna aln6-pe.sam > aln6.bam
 
-#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
 
 #General code template
 samtools sort test.bam test_sorted
